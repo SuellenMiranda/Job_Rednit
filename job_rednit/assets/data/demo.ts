c35ea26d412f9de31b64c11fa8b922301389eac0
Suh <suellen.org@gmail.com>
2022-09-25 14:32:48 -0300

    Add files via upload

---
+++ job_rednit/assets/data/demo.ts
@@ -0,0 +1,132 @@
+import { DataT } from "../../types";
+import IMAGE_01 from "../images/01.jpeg";
+import IMAGE_02 from "../images/02.jpg";
+import IMAGE_03 from "../images/03.jpg";
+import IMAGE_04 from "../images/04.jpg";
+import IMAGE_05 from "../images/05.jpeg";
+import IMAGE_06 from "../images/06.webp";
+import IMAGE_07 from "../images/07.webp";
+import IMAGE_08 from "../images/08.jpeg";
+import IMAGE_09 from "../images/09.jpg";
+import IMAGE_10 from "../images/10.jpeg";
+
+const data: DataT[] = [
+  {
+    id: 1,
+    name: "go horse team",
+    isOnline: true,
+    match: "78",
+    description:
+      "Empresa focada em nosso profissionais, sem eles ela não exixtiria.",
+    message:
+      "Wow! We like you! would you accept to be called for an interview?",
+    image: IMAGE_01,
+  },
+  {
+    id: 2,
+    name: "SquadHelp",
+    match: "93",
+    description:
+      "Empresa em conjunta com os funcionários, com um ambiente amigável para um bom desenvolvimento.",
+    isOnline: false,
+    message:
+      "Wow! We like you! would you accept to be called for an interview?",
+    image: IMAGE_02,
+  },
+  {
+    id: 3,
+    name: "Dot-o-mato",
+    match: "45",
+    description:
+      "Empresa cede-15120, bem localizado, estrutura impecável, externamente arborizada e confortável.",
+    isOnline: false,
+    message:
+      "Wow! We like you! would you accept to be called for an interview?",
+    image: IMAGE_03,
+  },
+  {
+    id: 4,
+    name: "Ivellios Tramys",
+    match: "88",
+    description:
+      "Empresa de tecnologia, nosso foco, disponibilidade presencial e home officer, nosso foco são nossos projetos em conjunto com o funcionário confortavel com 100% de flexibilidade.",
+    isOnline: true,
+    message:
+      "Wow! We like you! would you accept to be called for an interview?",
+    image: IMAGE_04,
+  },
+  {
+    id: 5,
+    name: "Gilhad Greynore",
+    match: "76",
+    description:
+      "Empresa com foco em brainstorm, e ajudar de todos. O que for melhor para todos é o nosso lema.",
+    isOnline: false,
+    message:
+      "Wow! We like you! would you accept to be called for an interview?",
+    image: IMAGE_05,
+  },
+  {
+    id: 6,
+    name: "Elora Yeszeiros",
+    match: "95",
+    description:
+      "Empresa convencional, focada, determinada. Venha você também ser determinado com a gente.",
+    isOnline: true,
+    message:
+      "Wow! We like you! would you accept to be called for an interview?",
+    image: IMAGE_06,
+  },
+  {
+    id: 7,
+    name: "Jancaryn Miapetor",
+    match: "67",
+    description:
+      "Nosso foco é como conversa entre amigos e não formal como empresa convencional.",
+    isOnline: true,
+    message:
+      "Wow! We like you! would you accept to be called for an interview?",
+    image: IMAGE_07,
+  },
+  {
+    id: 8,
+    name: "Abrantes",
+    match: "100",
+    description:
+      "Just me, just me. Its Abrantes",
+    age: "xx",
+    location: "Irvine, CA",
+    info1: 'Straight, Single, 5"10',
+    info2: "inovador, criativo & persistente",
+    info3: "Beaches, Mountain, Cafe, Movies",
+    info4: "Last seen: 23h ago",
+    isOnline: true,
+    message:
+      "Ops, To save for me.",
+    image: IMAGE_08,
+  },
+  {
+    id: 9,
+    name: "Hireth Preszeiros",
+    match: "74",
+    description:
+      "Empresa solar! Sim vendas de ernegia solar.",
+    isOnline: true,
+    message:
+      "Wow! We like you! would you accept to be called for an interview?",
+    image: IMAGE_09,
+  },
+  {
+    id: 10,
+    name: "flick",
+    match: "98",
+    description:
+      "Start Up com intenção a interação total entre todos os funcionários, venha fazer parte junto.",
+    isOnline: false,
+    message:
+      "Wow! We like you! would you accept to be called for an interview?",
+    image: IMAGE_10,
+  },
+];
+
+export default data;
